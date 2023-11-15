dad44b581f841892a0fb58237b599b196f536cf0
Gabriel Sena <gsdcdj@gmail.com>
2023-11-15 15:31:16 -0300

    :sparkles: componente e children

--- ui-twitter/src/components/Tweet.tsx
+++ ui-twitter/src/components/Tweet.tsx
@@ -3,7 +3,11 @@ export function Tweet(props) {
   return (
     <div>
       <strong>{props.user}</strong>
+<<<<<<< Updated upstream
       <p>{props.content}</p>
+=======
+      <p>{props.children}</p>
+>>>>>>> Stashed changes
       <button>Like</button>
     </div>
   )
--- ui-twitter/src/main.tsx
+++ ui-twitter/src/main.tsx
@@ -5,9 +5,23 @@ import { Tweet } from './components/Tweet'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>
+<<<<<<< Updated upstream
       <Tweet user="Rodrigo" content="Meu primeiro tweet" />
       <Tweet user="Gabriel" content="Olá, Mundo!" />
       <Tweet user="Carla" content="Testee" />
+=======
+      <Tweet user="Rodrigo">
+        Meu primeiro tweet
+      </Tweet>
+
+      <Tweet user="Gabriel">
+        Olá, Mundo!
+      </Tweet>
+      
+      <Tweet user="Carla">
+        Testee
+      </Tweet>
+>>>>>>> Stashed changes
     </div>
   </React.StrictMode>,
 )
