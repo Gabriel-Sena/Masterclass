381e9a418373fc914d50163fe42dc6646c829158
Gabriel Sena <gsdcdj@gmail.com>
2023-12-08 12:28:52 -0300

    :sparkles: fundamentos css .v3

--- ui-twitter/src/main.tsx
+++ ui-twitter/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Tweet } from './components/Tweet'
 
+import './global.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>
